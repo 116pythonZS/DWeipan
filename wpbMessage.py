46dc75616cd0b79d01670f5621171b51f592e1a2
HXL <huxueli1986@183.com>
2016-08-31 00:45:11 +0800

    message

--- wpbMessage.py
+++ wpbMessage.py
@@ -7,9 +7,49 @@
     @time   : 16/8/30 20:52
 """
 
+# typedef struct
+# {
+#     int wCmd; //命令号
+#     int wSeq; //包的序列号,业务使用,转发不再关心
+#     unsigned char  bEncryptFlag; //加密标志(0: 不 加密1:加密 )
+#     int wKeySeq;
+#     unsigned char  sReserved[7];
+# } WeixinPkgHead; //协议包头
+# typedef struct
+# {
+#     WeixinPkgHead stWeixinPkgHead;
+#     unsigned char   sBody[0];
+# } WeixinPkg; //协议包
+#
+# 数据包长度(4+20+包体长度) + 包头 + 包体
+
+import struct
+import json
+
+HEADFMT = '!iiBI7B'
 class WPBMessage(object):
-	msgSeq = 0
-	def __init__(self):
-		self.cmdNO = 0
-		self.encryFlag = 1
-		pass
+	def __init__(self, data, seq):
+		self.encrpyData = data
+		self.msgSeq = seq
+		self._unpack()
+	
+	def _unpack(self):
+		s = struct.Struct(HEADFMT)
+		head = s.unpack_from(self.encrpyData, 0)
+		self.cmdNo = head[0]
+		self.seq = head[1]
+		self.encrpyFlag = head[2]
+		self._decryp()
+		
+	def _decryp(self):
+		s = struct.Struct(HEADFMT)
+		body = self.encrpyData[s.size:]
+		if self.encrpyFlag:
+			pass
+		else:
+			self.decrpyData = json.loads(body)
+			
+	def getResult(self):
+		return self.decrpyData
+		
+		
--- wpbSocket.py
+++ wpbSocket.py
@@ -12,12 +12,30 @@ from time import ctime
 import json
 import chardet
 import struct
+import wpbMessage
 
 PACKAGELEN = 4      # 包长
 PACKHEADLEN = 20    # 包头长
 BUFSIZE = 1024
 HOST = ("wptest.baidao.com", 9103)
 
+seq = 0
+def test():
+	try:
+		svr = socket(AF_INET, SOCK_STREAM)
+		svr.connect(HOST)
+		while True:
+			totalLen = svr.recv(PACKAGELEN)
+			length = struct.unpack('!i', totalLen)
+			data = svr.recv(length[0] - PACKAGELEN)
+			# data = svr.recv(length[0] - PACKAGELEN - PACKHEADLEN)
+			# print ctime() + '收到数据:Len[%d]' % length
+			msg = wpbMessage.WPBMessage(data, 0)
+			print msg.getResult()
+		# print json.dumps(data)
+	except Exception, e:
+		print e
+		
 def main():
 	try:
 		svr = socket(AF_INET, SOCK_STREAM)
@@ -38,4 +56,4 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+	test()
